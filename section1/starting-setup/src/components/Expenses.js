effed2ff0c640231d7e931133be771a4f5741e8f
neb <10528790@my.uvu.edu>
2021-06-14 18:23:05 -0600

    added card component, used children props

--- section1/starting-setup/src/components/Expenses.js
+++ section1/starting-setup/src/components/Expenses.js
@@ -1,8 +1,9 @@
+import Card from './Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.expenses.map((expense) => {
         return (
           <ExpenseItem
@@ -13,7 +14,7 @@ function Expenses(props) {
           />
         );
       })}
-    </div>
+    </Card>
   );
 }
 
