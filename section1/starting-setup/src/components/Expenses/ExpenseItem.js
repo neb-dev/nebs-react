272af206e33a314bb62026c59b19551db8617c05
neb <jneb.dev@outlook.com>
2021-07-13 19:51:10 -0600

    added useState hook to update expense title

--- section1/starting-setup/src/components/Expenses/ExpenseItem.js
+++ section1/starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,24 @@
+import { useState } from "react";
+
 import "../Expenses/ExpenseItem.css";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked");
-  }
+    setTitle("Updated");
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={clickHandler}>Update</button>
     </Card>
   );
 };
