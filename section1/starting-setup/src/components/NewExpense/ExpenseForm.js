1195e3bbc1b99137da10fbdb792f24c9ebf09455
neb <jneb.dev@outlook.com>
2021-07-14 21:44:30 -0600

    two-way binding

--- section1/starting-setup/src/components/NewExpense/ExpenseForm.js
+++ section1/starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -58,12 +58,28 @@ const ExpenseForm = () => {
     // });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: inputTitle,
+      amount: inputAmount,
+      date: new Date(inputDate)
+    }
+
+    console.log(expenseData);
+
+    setInputTitle('');
+    setInputAmount('');
+    setInputDate('');
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={inputTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -71,6 +87,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={inputAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -80,6 +97,7 @@ const ExpenseForm = () => {
             type="date"
             min="1776-01-01"
             max="2100-12-31"
+            value={inputDate}
             onChange={dateChangeHandler}
           />
         </div>
