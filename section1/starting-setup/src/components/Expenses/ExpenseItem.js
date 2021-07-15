3d03495c5f1d1b0306359111aa46b1838008a6ea
neb <jneb.dev@outlook.com>
2021-07-15 17:53:06 -0600

    removed button from expenseitem component

--- section1/starting-setup/src/components/Expenses/ExpenseItem.js
+++ section1/starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,15 @@
-import { useState } from "react";
-
 import "../Expenses/ExpenseItem.css";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  console.log("ExpenseItem instance created.");
-
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log(title);
-    console.log("ExpenseItem instance evaluated.")
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Update</button>
     </Card>
   );
 };
