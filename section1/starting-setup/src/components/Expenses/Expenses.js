e8abec6552d9825844cc39265430c2d3fbd88625
neb <jneb.dev@outlook.com>
2021-07-14 22:32:41 -0600

    added expensesfilter component, useState hook to store selected expense filter

--- section1/starting-setup/src/components/Expenses/Expenses.js
+++ section1/starting-setup/src/components/Expenses/Expenses.js
@@ -1,23 +1,37 @@
+import { useState } from "react";
+
 import Card from "../UI/Card";
 import ExpenseItem from "../Expenses/ExpenseItem";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import "../Expenses/Expenses.css";
 
 const Expenses = (props) => {
+  const [inputFilter, setInputFilter] = useState("");
+
+  const saveFilterHandler = (inputFilter) => {
+    setInputFilter(inputFilter);
+    console.log(inputFilter);
+  }
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => {
-        return (
-          <div className="container">
-            <ExpenseItem
-              date={expense.date}
-              title={expense.title}
-              amount={expense.amount}
-              key={expense.id}
-            />
-          </div>
-        );
-      })}
-    </Card>
+    <div>
+      <ExpensesFilter onSaveFilter={saveFilterHandler} />
+
+      <Card className="expenses">
+        {props.expenses.map((expense) => {
+          return (
+            <div className="container">
+              <ExpenseItem
+                date={expense.date}
+                title={expense.title}
+                amount={expense.amount}
+                key={expense.id}
+              />
+            </div>
+          );
+        })}
+      </Card>
+    </div>
   );
 };
 
